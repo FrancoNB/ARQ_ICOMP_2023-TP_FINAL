ff0dccf38bc70a4ef6ed2650b728b1ebfe141b38
francobottini99 <francobottini99@mi.unc.edu.ar>
2023-11-23 10:58:04 -0300

    Ajustes ALU.
    Refactorización de la ALU para incluir las nuevas operaciones. Cambio en el manejo de los códigos de operación para introducir el soporte de control.

--- vivado.src/sources/ex/inc/ALU.vh
+++ vivado.src/sources/ex/inc/ALU.vh
@@ -1,14 +1,21 @@
 `ifndef __ALU_VH__
 `define __ALU_VH__
-    `define OP_ADD   6'b100000
-    `define OP_SUB   6'b100010
-    `define OP_AND   6'b100100
-    `define OP_OR    6'b100101
-    `define OP_XOR   6'b100110
-    `define OP_SRA   6'b000011
-    `define OP_SRL   6'b000010
-    `define OP_NOR   6'b100111
+    `define CTR_SLL   4'b0000 // Shift left logical
+    `define CTR_SRL   4'b0001 // Shift right logical
+    `define CTR_SRA   4'b0010 // Shift right arithmetic
+    `define CTR_ADD   4'b0011 // Sum
+    `define CTR_SUB   4'b0100 // Substract 
+    `define CTR_AND   4'b0101 // Logical and
+    `define CTR_OR    4'b0110 // Logical or
+    `define CTR_XOR   4'b0111 // Logical xor
+    `define CTR_NOR   4'b1000 // Logical nor
+    `define CTR_SLT   4'b1001 // Set if less than
+    `define CTR_SLLV  4'b1010 // Shift left logical
+    `define CTR_SRLV  4'b1011 // Shift right logical
+    `define CTR_SRAV  4'b1100 // Shift right arithmetic
+    `define CTR_SLL16 4'b1101 // Shift left logical 16
+    `define CTR_NEQ   4'b1110 // Equal
     
-    `define OP_CODE_WIDTH 6
-    `define IO_BUS_WIDTH 8
+    `define CTR_BUS_WIDTH 4
+    `define IO_BUS_WIDTH  8
 `endif // __ALU_VH__
--- vivado.src/sources/ex/src/ALU.v
+++ vivado.src/sources/ex/src/ALU.v
@@ -5,29 +5,36 @@
 module alu
     #(
         parameter IO_BUS_WIDTH  = `IO_BUS_WIDTH,
-        parameter OP_CODE_WIDTH = `OP_CODE_WIDTH
+        parameter CTR_BUS_WIDTH = `CTR_BUS_WIDTH
     )
     (
-        input         [OP_CODE_WIDTH - 1 : 0] op_code, 
+        input         [CTR_BUS_WIDTH - 1 : 0] ctr_code, 
         input  signed [IO_BUS_WIDTH - 1 : 0]  data_a,
         input  signed [IO_BUS_WIDTH - 1 : 0]  data_b,
         output signed [IO_BUS_WIDTH - 1 : 0]  out_data
     );
     
     reg[IO_BUS_WIDTH - 1 : 0] result;
-    
+
     always@(*)
     begin
-        case(op_code)
-            `OP_ADD  : result <= data_a + data_b;
-            `OP_SUB  : result <= data_a - data_b;
-            `OP_AND  : result <= data_a & data_b;
-            `OP_OR   : result <= data_a | data_b;
-            `OP_XOR  : result <= data_a ^  data_b;
-            `OP_SRA  : result <= data_a >>> data_b;
-            `OP_SRL  : result <= data_a >> data_b;
-            `OP_NOR  : result <= ~(data_a | data_b);
-            default  : result <= {IO_BUS_WIDTH {1'bz}};
+        case(ctr_code)
+            `CTR_SLL   : result = data_a << data_b;
+            `CTR_SRL   : result = data_a >> data_b;
+            `CTR_SRA   : result = data_a >>> data_b;
+            `CTR_ADD   : result = data_a + data_b;
+            `CTR_SUB   : result = data_a - data_b;
+            `CTR_AND   : result = data_a & data_b;
+            `CTR_OR    : result = data_a | data_b;
+            `CTR_XOR   : result = data_a ^ data_b;
+            `CTR_NOR   : result = ~(data_a | data_b);
+            `CTR_SLT   : result = data_a < data_b;
+            `CTR_SLLV  : result = data_a << data_b;
+            `CTR_SRLV  : result = data_a >> data_b;
+            `CTR_SRAV  : result = data_a >>> data_b;
+            `CTR_SLL16 : result = data_a << 16;
+            `CTR_NEQ   : result = (data_a == data_b);
+            default    : result = {IO_BUS_WIDTH {1'bz}};
         endcase
     end
     
