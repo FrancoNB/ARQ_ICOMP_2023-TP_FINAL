c3fb504ea4a0864798b93081fdf9e447b6658742
francobottini99 <francobottini99@mi.unc.edu.ar>
2023-12-04 11:01:01 -0300

    Implementación EX_MEM. Ajustes IF_ID.
    Se implementa la etapa del pipeline EX a MEM. Se realizan ajustes en la etapa IF_ID.

--- vivado.src/sources/pipeline/src/if_id.v
+++ vivado.src/sources/pipeline/src/if_id.v
@@ -5,27 +5,27 @@
 module if_id
     #(
         parameter PC_SIZE  = `DEFAULT_PC_SIZE,
-        parameter BUS_SIZE = `DEFAULT_ID_BUS_SIZE
+        parameter INSTRUCTION_SIZE = `DEFAULT_ID_INSTRUCTION_SIZE
     )
     (
-        input  wire                    i_clk,
-        input  wire                    i_reset,
-        input  wire                    i_enable,
-        input  wire [PC_SIZE - 1 : 0]  i_next_seq_pc,
-        input  wire [BUS_SIZE - 1 : 0] i_instruction,
-        output wire [PC_SIZE - 1 : 0]  o_next_seq_pc,
-        output wire [BUS_SIZE - 1 : 0] o_instruction
+        input  wire                            i_clk,
+        input  wire                            i_reset,
+        input  wire                            i_enable,
+        input  wire [PC_SIZE - 1 : 0]          i_next_seq_pc,
+        input  wire [INSTRUCTION_SIZE - 1 : 0] i_instruction,
+        output wire [PC_SIZE - 1 : 0]          o_next_seq_pc,
+        output wire [INSTRUCTION_SIZE - 1 : 0] o_instruction
     );
 
     reg [PC_SIZE - 1 : 0]          next_seq_pc;
-    reg [BUS_SIZE - 1 : 0] instruction;
+    reg [INSTRUCTION_SIZE - 1 : 0] instruction;
 
     always @(posedge i_clk or posedge i_reset) 
     begin
         if (i_reset)
             begin
                 next_seq_pc <= `CLEAR(PC_SIZE);
-                instruction <= `CLEAR(BUS_SIZE);
+                instruction <= `CLEAR(INSTRUCTION_SIZE);
             end
         else if (i_enable)
             begin
